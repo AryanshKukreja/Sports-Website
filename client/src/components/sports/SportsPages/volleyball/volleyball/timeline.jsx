f9a4c86dcc790a0be13d1ed15d12ba5be6d8de60
ShubhSarin <shubhsarin08@gmail.com>
2025-07-10 01:03:14 +0530

    UI Changes

--- client/src/components/sports/SportsPages/volleyball/volleyball/timeline.jsx
+++ client/src/components/sports/SportsPages/volleyball/volleyball/timeline.jsx
@@ -13,29 +13,34 @@ const Timel = () => {
                 <VerticalTimelineElement className="vertical-timeline-element--education"
                 iconStyle={{background:'#000', color:'#fff'}}
                 icon={<GiTrophyCup />}
-                date='2022-23' contentStyle={{ background: '#2F2B77', color: '#fff' }}>
-                    <h3 style={{color: '#E4E2FA'}}>Inter IIt Aquatics Meet</h3>
-                    <p style={{color: '#E4E2FA'}}>
-                        Mens Swimming : 2nd place
-                        <br/>Mens Water Polo : 4th place
-                        <br/>Best Swimmer Award : Kaushal Khutneta
-                    </p>
+                date='2022-23' contentStyle={{ background: '#2F2B77', color: '#fff' }}
+                style={{ boxShadow: '0' }}>
+                    <h3>Inter IIt Aquatics Meet</h3>
+                    <table style={{ width: '100%', borderCollapse: 'collapse', margin: 0, padding: 0 }}>
+                        <tbody>
+                            <tr><td>Mens Swimming</td><td>2nd place</td></tr>
+                            <tr><td>Mens Water Polo</td><td>4th place</td></tr>
+                            <tr><td>Best Swimmer Award</td><td>Kaushal Khutneta</td></tr>
+                        </tbody>
+                    </table>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement className="vertical-timeline-element--education"
                 iconStyle={{background:'#000', color:'#fff'}}
                 icon={<GiMedal />}
                 date='2022-23' contentStyle={{ background: '#2F2B77', color: '#fff' }}>
-                    <h3 style={{ fontSize:'25px', color: '#E4E2FA'}}>Inter IIt Aquatics Meet</h3>
+                    <h3 style={{ fontSize:'25px'}}>Inter IIt Aquatics Meet</h3>
                     {/* <h5 style={{color: '#000', wordSpacing: '40px', fontSize:'20px'}}>Events Position Players</h5> */}
-                    <p style={{color: '#E4E2FA'}}>
-                        200m Breastroke(M) &nbsp;&nbsp;&nbsp; Gold &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kushal Khunteta<br/>
-                        100m Breastroke(M) &nbsp;&nbsp;&nbsp; Gold &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kushal Khunteta<br/>
-                        50m Breastroke(M) &nbsp;&nbsp;&nbsp; Silver &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kushal Khunteta<br/>
-                        100m Butterfly(M) &nbsp;&nbsp;&nbsp; Gold &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vaishnav Rao<br/>
-                        50m Butterfly(M) &nbsp;&nbsp;&nbsp; Silver &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vaishnav Rao<br/>
-                        200m Medley (M) &nbsp;&nbsp;&nbsp; Silver &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vaishnav Rao<br/>
-                        100m Freestyle(W) &nbsp;&nbsp;&nbsp; Gold &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mansi Khedekar<br/>
-                    </p>
+                    <table style={{ width: '100%', borderCollapse: 'collapse', margin: 0, padding: 0 }}>
+                        <tbody>
+                            <tr><td>200m Breastroke(M)</td><td>Gold</td><td>Kushal Khunteta</td></tr>
+                            <tr><td>100m Breastroke(M)</td><td>Gold</td><td>Kushal Khunteta</td></tr>
+                            <tr><td>50m Breastroke(M)</td><td>Silver</td><td>Kushal Khunteta</td></tr>
+                            <tr><td>100m Butterfly(M)</td><td>Gold</td><td>Vaishnav Rao</td></tr>
+                            <tr><td>50m Butterfly(M)</td><td>Silver</td><td>Vaishnav Rao</td></tr>
+                            <tr><td>200m Medley (M)</td><td>Silver</td><td>Vaishnav Rao</td></tr>
+                            <tr><td>100m Freestyle(W)</td><td>Gold</td><td>Mansi Khedekar</td></tr>
+                        </tbody>
+                    </table>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement className="vertical-timeline-element--education"
                 iconStyle={{background:'#000', color:'#fff'}}
@@ -51,13 +56,15 @@ const Timel = () => {
                 iconStyle={{background:'#000', color:'#fff'}}
                 icon={<GiMedal />}
                 date='2023' contentStyle={{ background: '#2F2B77', color: '#fff' }}>
-                    <h3 style={{ fontSize:'25px', color: '#E4E2FA'}}>Inter IIt Aquatics Meet</h3>
+                    <h3 style={{ fontSize:'25px'}}>Inter IIt Aquatics Meet</h3>
                     {/* <h5 style={{color: '#000', wordSpacing: '40px', fontSize:'20px'}}>Events Position Players</h5> */}
-                    <p style={{color: '#E4E2FA'}}>
-                        100m Freestyle(M) &nbsp;&nbsp;&nbsp; Bronze &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Suyash Bhandare<br/>
-                        200m Breastroke(M) &nbsp;&nbsp;&nbsp; Silver &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kartik Akappa<br/>
-                        200m Freestyle(M) &nbsp;&nbsp;&nbsp; 4th place &nbsp;&nbsp;&nbsp;&nbsp;Suyash Bhandare<br/>
-                    </p>
+                    <table style={{ width: '100%', borderCollapse: 'collapse', margin: 0, padding: 0 }}>
+                        <tbody>
+                            <tr><td>100m Freestyle(M)</td><td>Bronze</td><td>Suyash Bhandare</td></tr>
+                            <tr><td>200m Breastroke(M)</td><td>Silver</td><td>Kartik Akappa</td></tr>
+                            <tr><td>200m Freestyle(M)</td><td>4th place</td><td>Suyash Bhandare</td></tr>
+                        </tbody>
+                    </table>
                 </VerticalTimelineElement>
             </VerticalTimeline>
             
